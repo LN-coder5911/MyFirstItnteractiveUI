7fe00e087bd134e9ca9ea9bc93f6a07b043ab814
Lena Nichols <lena.k.nichols@gmail.com>
2025-07-23 14:24:30 -0700

    Initial Commit

---
+++ MyFirstItnteractiveUI/MyFirstItnteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstItnteractiveUIApp.swift
+//  MyFirstItnteractiveUI
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstItnteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
